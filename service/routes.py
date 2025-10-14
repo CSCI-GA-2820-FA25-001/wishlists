7fd5168c3e559a11b2bb24aa570858160cff1140
Jiangchen (Alvin) Li <59038073+JcxaL@users.noreply.github.com>
2025-10-13 23:27:11 -0400

    Implement root route and w/t service metadata (#35)
    Co-authored-by: Dingwen Wang <dingwen.w@nyu.edu>

--- service/routes.py
+++ service/routes.py
@@ -38,8 +38,21 @@ STATE_CUSTOMER_ID = 1
 @app.route("/")
 def index():
     """Root URL response"""
+    app.logger.info("Request for service metadata")
     return (
-        "Reminder: return some useful information in json format about the service here",
+        jsonify(
+            {
+                "name": "wishlists-service",
+                "version": "0.1.0",
+                "endpoints": [
+                    "/wishlists",
+                    "/wishlists/{id}",
+                    "/wishlists/{id}/items",
+                    "/wishlists/{id}/items/{item_id}",
+                ],
+                "docs": "See README for examples",
+            }
+        ),
         status.HTTP_200_OK,
     )
 
