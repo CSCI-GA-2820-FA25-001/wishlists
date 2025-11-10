cf6fae4e466e6feb2fbbb30df62c684f9bc1eca6
Xiao-1231751 <xiaohuhua2022@gmail.com>
2025-11-10 00:37:56 +0000

    add ui code for wishlist

---
+++ service/static/js/rest_api.js
@@ -0,0 +1,107 @@
+$(function () {
+
+    // ****************************************
+    //  U T I L I T Y   F U N C T I O N S
+    // ****************************************
+
+    // Updates the form with data from the response
+    function update_form_data(res) {
+        $("#wishlist_id").val(res.id);
+        $("#wishlist_name").val(res.name);
+        $("#wishlist_category").val(res.category);
+        $("#wishlist_description").val(res.description);
+        $("#wishlist_customer_id").val(res.customer_id);
+        $("#wishlist_created_date").val(res.created_date);
+        $("#wishlist_updated_date").val(res.updated_date);
+    }
+
+    /// Clears all form fields
+    function clear_form_data() {
+        $("#wishlist_name").val("");
+        $("#wishlist_category").val("");
+        $("#wishlist_customer_id").val("");
+        $("#wishlist_description").val("");
+        $("#wishlist_created_date").val("");
+        $("#wishlist_updated_date").val("");
+    }
+
+    // Updates the flash message area
+    function flash_message(message) {
+        $("#flash_message").empty();
+        $("#flash_message").append(message);
+    }
+
+    // ****************************************
+    // Create a Wishlist
+    // ****************************************
+
+    $("#create-btn").click(function () {
+
+    });
+
+    // ****************************************
+    // Update a Wishlist
+    // ****************************************
+
+    $("#update-btn").click(function () {
+        
+    });
+
+
+    // ****************************************
+    // Retrieve a Wishlist
+    // ****************************************
+
+    $("#retrieve-btn").click(function () {
+
+        let wishlist_id = $("#wishlist_id").val();
+
+        $("#flash_message").empty();
+
+        let ajax = $.ajax({
+            type: "GET",
+            url: `/wishlists/${wishlist_id}`,
+            contentType: "application/json",
+            data: ''
+        })
+
+        ajax.done(function(res){
+            //alert(res.toSource())
+            update_form_data(res)
+            flash_message("Success")
+        });
+
+        ajax.fail(function(res){
+            clear_form_data()
+            flash_message(res.responseJSON.message)
+        });
+
+    });
+
+    // ****************************************
+    // Delete a Wishlist
+    // ****************************************
+
+    $("#delete-btn").click(function () {
+    
+    });
+
+    // ****************************************
+    // Clear the form
+    // ****************************************
+
+    $("#clear-btn").click(function () {
+        $("#wishlist_id").val("");
+        $("#flash_message").empty();
+        clear_form_data()
+    });
+
+    // ****************************************
+    // Search for a Wishlist
+    // ****************************************
+
+    $("#search-btn").click(function () {
+
+    });
+
+})
